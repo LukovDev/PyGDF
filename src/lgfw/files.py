6df52215d3b921b56aeb4e804bb7aed6f010ee5a
LukovDev <ruslanfelitsan@gmail.com>
2024-03-18 02:44:05 +0300

    commit 18 march 2024

--- src/lgfw/files.py
+++ src/lgfw/files.py
@@ -6,6 +6,8 @@
 # Импортируем:
 if True:
     import json
+    import tkinter as tk
+    from tkinter import filedialog
     from .graphics import Image, Texture
 
 
@@ -54,3 +56,14 @@ def load_json(file_path: str, mode: str = "r+", encoding: str = "utf-8") -> dict
 # Сохраняем json файл:
 def save_json(file_path: str, data: dict, mode: str = "w+", encoding: str = "utf-8", indent: int = 4) -> None:
     with open(file_path, mode, encoding=encoding) as f: json.dump(data, f, indent=indent)
+
+
+# Выбрать файл:
+def get_file_path(file_types: list[tuple] = [("All:" "*.*")], icon_path: str = "") -> str:
+    root = tk.Tk() ; root.withdraw()
+    try: root.iconbitmap(icon_path)
+    except Exception as error: pass
+    if file_types: fp = filedialog.askopenfilename(filetypes=file_types)
+    else: fp = filedialog.askopenfilename()
+    root.destroy()
+    return fp

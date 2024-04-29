29277fda0f5a48021728d8ec6abf5e59d162e771
LukovDev <ruslanfelitsan@gmail.com>
2024-04-29 21:25:03 +0300

    commit 29 april 2024 21:25

--- src/gdf/graphics/font.py
+++ src/gdf/graphics/font.py
@@ -5,6 +5,7 @@
 
 # Импортируем:
 if True:
+    import os
     import pygame
     from .image import Image
     from .texture import Texture
@@ -27,35 +28,49 @@ class Font:
         if font_path is not None and os.path.isfile(font_path):
             self.font = pygame.font.Font(font_path, font_size)
         else: self.font = pygame.font.SysFont("Arial", font_size)
+        self.texture = None
 
     # Получить спрайт текста:
     def get_sprite(self, text: str, color: list, bg_color: list = None,
                    offset_x: int = 0, offset_y: int = 0, smooth: bool = True) -> Sprite2D:
         if bg_color is None: bg_color = [0, 0, 0, 0]
+        bg_color = bg_color[0]*255, bg_color[1]*255, bg_color[2]*255, bg_color[3]*255
+        color = color[0]*255, color[1]*255, color[2]*255, color[3]*255
         srf = self.font.render(text, smooth, color[:3])
         srf.set_alpha(color[3])
         srf_size = srf.get_width()+offset_x*2, srf.get_height()+offset_y*2
         bg_surface = pygame.Surface(srf_size, pygame.SRCALPHA)
         bg_surface.fill(bg_color)
         bg_surface.blit(srf, (offset_x, offset_y))
-        texture = Texture(Image(surface=bg_surface))
-        return Sprite2D(texture)
+        if self.texture is not None:
+            self.texture.destroy()
+            self.texture = None
+        if self.texture is None:
+            self.texture = Texture(Image(surface=bg_surface))
+        return Sprite2D(self.texture)
 
 
 # Класс системного шрифта:
 class SysFont:
     def __init__(self, font_name: str, font_size: int) -> None:
         self.font = pygame.font.SysFont(font_name, font_size)
+        self.texture = None
 
     # Получить спрайт текста:
     def get_sprite(self, text: str, color: list, bg_color: list = None,
                    offset_x: int = 0, offset_y: int = 0, smooth: bool = True) -> Sprite2D:
         if bg_color is None: bg_color = [0, 0, 0, 0]
+        bg_color = bg_color[0]*255, bg_color[1]*255, bg_color[2]*255, bg_color[3]*255
+        color = color[0]*255, color[1]*255, color[2]*255, color[3]*255
         srf = self.font.render(text, smooth, color[:3])
         srf.set_alpha(color[3])
         srf_size = srf.get_width()+offset_x*2, srf.get_height()+offset_y*2
         bg_surface = pygame.Surface(srf_size, pygame.SRCALPHA)
         bg_surface.fill(bg_color)
         bg_surface.blit(srf, (offset_x, offset_y))
-        texture = Texture(Image(surface=bg_surface))
-        return Sprite2D(texture)
+        if self.texture is not None:
+            self.texture.destroy()
+            self.texture = None
+        if self.texture is None:
+            self.texture = Texture(Image(surface=bg_surface))
+        return Sprite2D(self.texture)

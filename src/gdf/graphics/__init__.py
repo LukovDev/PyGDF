148518eff0b0b8254f51149b20a497c047d44356
LukovDev <ruslanfelitsan@gmail.com>
2024-11-09 18:59:03 +0300

    commit 09 november 2024 18:58

--- src/gdf/graphics/__init__.py
+++ src/gdf/graphics/__init__.py
@@ -64,7 +64,7 @@ from .image     import Image
 from .imgui     import ImGUI
 from .light     import Light2D
 from .packer    import PackerTexture
-from .particles import SimpleParticleEffect2D
+from .particles import ParticleEffect2D
 from .renderer  import Renderer2D
 from .scene     import Scene
 from .shader    import ShaderProgram
